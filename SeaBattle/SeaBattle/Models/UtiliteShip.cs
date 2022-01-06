aaab6af399143f64f558c3998bf86abb14b4c37f
DESKTOP-HO5O0U5\leoon <leoonovn@gmail.com>
2022-01-06 13:40:47 +0200

    Added utilite ship model

---
+++ SeaBattle/SeaBattle/Models/UtiliteShip.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SeaBattle.Enums;
+using SeaBattle.Models.Abstractions;
+
+namespace SeaBattle.Models
+{
+    internal class UtiliteShip : Ship, IUtilityShip
+    {
+        public UtiliteShip(int length, int speed, int actionRange) : base(length, speed, actionRange)
+        {
+        }
+
+        public override void Move(Direction direction)
+        {
+            throw new NotImplementedException("The logic of movement is not implemented");
+        }
+
+        public void Repair()
+        {
+            throw new NotImplementedException("The logic of shooting is not implemented");
+        }
+    }
+}
